8f4dcc56b5ba44e7405a9fd25e162958ef1b468c
andrehio <ahdreki@gmail.com>
2022-11-02 10:28:53 -0300

    11 - Função

---
+++ php/php_0011_function.php
@@ -0,0 +1,16 @@
+<?php
+// Função
+
+function totalpagamento($totalprodutos, $frete)
+{
+    $custototal = 0;
+    for ($indiceprod = 0; $indiceprod < count($totalprodutos); $indiceprod++) {
+        $custototal = $custototal + $totalprodutos[$indiceprod];
+    }
+    $custototal += $frete;
+    return $custototal;
+}
+
+$produtos = array(50, 200, 300);
+echo "Custo total: R$ " . totalpagamento($produtos, 30);
+?>
