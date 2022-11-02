21c47395e6ef80660ce63d48bc2e2d8bc104e4f5
andrehio <ahdreki@gmail.com>
2022-11-02 10:15:20 -0300

    6 - Switch-Case

---
+++ php/php_0006_switch_case.php
@@ -0,0 +1,15 @@
+<?php
+// Estrutura Switch-Case
+
+$categoriaproduto = "Vídeo";
+switch ($categoriaproduto) {
+    case "Vídeo":
+        echo 'Bem-vindo ao setor de Vídeo.';
+        break;
+    case "Áudio":
+        echo 'Bem-vindo ao setor de Áudio.';
+        break;
+    default:
+        echo 'Bem-vindo ao setor geral.';
+}
+?>
