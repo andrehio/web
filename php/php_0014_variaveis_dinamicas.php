0b166834859cb8a19cd9be7ce26201c21cb3118d
andrehio <ahdreki@gmail.com>
2022-11-02 11:26:12 -0300

    14 - Variável Dinâmica

--- php/php_0014_variaveis_dinamicas.php
+++ php/php_0014_variaveis_dinamicas.php
@@ -2,5 +2,13 @@
 // Variáveis dinamicas
 $nome_var = "codigo";
 $codigo = 9000;
-echo "$nome_var = " . $$nome_var;
+echo "$nome_var = " . $$nome_var . "<br>";
+
+
+$nomes = array("codigo", "nome", "cidade");
+$cidade = "Vila Velha";
+$var = "nomes";
+echo ${ $nomes[2]} . "<br>";
+echo ${ $var}[1];
+
 ?>
